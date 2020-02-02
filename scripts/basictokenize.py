514bfbc56b2dec37212936d85ffb880c72902cd6
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2020-02-02 08:29:22 +0200

    Cleanup

--- scripts/basictokenize.py
+++ scripts/basictokenize.py
@@ -3,7 +3,6 @@
 import sys
 
 from bert.tokenization import BasicTokenizer
-# from tokenizers.pre_tokenizers import BertPreTokenizer
 
 
 def argparser():
@@ -20,16 +19,10 @@ def basic_tokenize(tokenizer, fn, options):
         for ln, l in enumerate(f, start=1):
             l = l.rstrip('\n')
             print(' '.join(tokenizer.tokenize(l)))
-            # tokenized = tokenizer.pre_tokenize(l)
-            # print(' '.join(t[0] for t in tokenized))
 
 
 def main(argv):
     args = argparser().parse_args(argv[1:])
-    # tokenizer = BertPreTokenizer.new(
-    #     strip_accents=args.uncased,
-    #     lowercase=args.uncased
-    # )
     tokenizer = BasicTokenizer(do_lower_case=args.uncased)
     for fn in args.file:
         basic_tokenize(tokenizer, fn, args)
